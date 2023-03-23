159561c50d37bd63ecdf8f488116eaf84957f2e0
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-03-23 12:15:36 -0500

    placePiece

--- TicTacToeBoard.java
+++ TicTacToeBoard.java
@@ -1,7 +1,7 @@
 public class TicTacToeBoard {
-    public int[][] board = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    public int[][] board = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
 
-    public boolean aValidMove(int x, int y) {
+    private boolean aValidMove(int x, int y) {
         if (board[x][y] != 0) {
             return false;
         } else {
@@ -9,43 +9,41 @@ public class TicTacToeBoard {
         }
     }
 
-    //1 = player1, 2 = player2 0 = empty
-    
+    // 1 = player1, 2 = player2 0 = empty
+
     public int getCellContents(int x, int y) {
         return board[x][y];
     }
-    
-    public boolean placePiece(int xPos, int yPos, int playerNum) {
+
+    public boolean placePiece(int xPos, int yPos) {
         int x = xPos;
         int y = yPos;
-        int player = playerNum;
 
-        if (board[x][y] == 0) {
-            board[x][y] = player;
+        if (aValidMove(x, y) == true)
             return true;
-        }
+        
         return false;
-        }
+    }
 
-        //1 = player1 win, 2 = player2 win, 0 = in progress, -1
+    // 1 = player1 win, 2 = player2 win, 0 = in progress, -1
 
-        public int getGameStatus() {
-            if (board[0][0] == 1 && board[0][1] == 1 && board[0][2] == 1) {
-                return 1;
-            } else if (board[1][0] == 1 && board[1][1] == 1 && board[1][2] == 1) {
-                return 1;
-            } else if (board[2][0] == 1 && board[2][1] == 1 && board[2][2] == 1) {
-                return 1;
-            } else if (board[0][0] == 1 && board[1][0] == 1 && board[2][0] == 1) {
-                return 1;
-            } else if (board[0][1] == 1 && board[1][1] == 1 && board[2][1] == 1) {
-                return 1;
-            } else if (board[0][2] == 1 && board[1][2] == 1 && board[2][2] == 1) {
-                return 1;
-            } else if (board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1) {
-                return 1;
-            } else if (board[2][0] == 1 && board[1][1] == 1 && board[0][2] == 1) {
-                return 1;
+    public int getGameStatus() {
+        if (board[0][0] == 1 && board[0][1] == 1 && board[0][2] == 1) {
+            return 1;
+        } else if (board[1][0] == 1 && board[1][1] == 1 && board[1][2] == 1) {
+            return 1;
+        } else if (board[2][0] == 1 && board[2][1] == 1 && board[2][2] == 1) {
+            return 1;
+        } else if (board[0][0] == 1 && board[1][0] == 1 && board[2][0] == 1) {
+            return 1;
+        } else if (board[0][1] == 1 && board[1][1] == 1 && board[2][1] == 1) {
+            return 1;
+        } else if (board[0][2] == 1 && board[1][2] == 1 && board[2][2] == 1) {
+            return 1;
+        } else if (board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1) {
+            return 1;
+        } else if (board[2][0] == 1 && board[1][1] == 1 && board[0][2] == 1) {
+            return 1;
         } else if (board[0][0] == 2 && board[0][1] == 2 && board[0][2] == 2) {
             return 2;
         } else if (board[1][0] == 2 && board[1][1] == 2 && board[1][2] == 2) {
@@ -63,8 +61,8 @@ public class TicTacToeBoard {
         } else if (board[2][0] == 2 && board[1][1] == 2 && board[0][2] == 2) {
             return 2;
         } else if (board[0][0] == 0 || board[0][1] == 0 || board[0][2] == 0
-        || board[1][0] == 0 || board[1][1] == 0 || board[1][2] == 0
-        || board[2][0] == 0 || board[2][1] == 0 || board[2][2] == 0) {
+                || board[1][0] == 0 || board[1][1] == 0 || board[1][2] == 0
+                || board[2][0] == 0 || board[2][1] == 0 || board[2][2] == 0) {
             return 0;
         } else {
             return -1;
